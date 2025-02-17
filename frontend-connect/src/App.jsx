714b2c3f4200a323bced1c500743c3a3366c9fc5
noelnaveen70 <noelnaveen70@gmail.com>
2025-02-17 22:06:31 +0530

    changes

--- frontend-connect/src/App.jsx
+++ frontend-connect/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import axios from "axios";
+import { AuthProvider } from "./components/AuthContext";
 
 import Home from "./components/Home";
 import Login from "./components/Login";
@@ -18,6 +19,14 @@ import Updatehospital from "./components/Updatehospital";
 import Updateschool from "./components/Updateschool";
 import Createschool from "./components/Createschool";
 import Viewschool from "./components/Viewschool";
+import JobVacancy from "./components/JobVacancy";
+import Tourism from "./components/Tourism";
+import Hospital from "./components/Hospital";
+import School from "./components/School";
+import Jobapply from "./components/Jobapply";
+import JobApplications from "./components/JobApplications";
+
+// Importing district components
 import Alappuzha from "./components/AllDistricts/Alappuzha";
 import Ernakulam from "./components/AllDistricts/Ernakulam";
 import Idukki from "./components/AllDistricts/Idukki";
@@ -32,14 +41,6 @@ import Pathanamthitta from "./components/AllDistricts/Pathanamthitta";
 import Thiruvananthapuram from "./components/AllDistricts/Thiruvananthapuram";
 import Thrissur from "./components/AllDistricts/Thrissur";
 import Wayanad from "./components/AllDistricts/Wayanad";
-
-import JobVacancy from "./components/JobVacancy";
-import Tourism from "./components/Tourism";
-import Hospital from "./components/Hospital";
-import School from "./components/School";
-import Jobapply from "./components/Jobapply";
-import JobApplications from "./components/JobApplications";
-import { AuthProvider } from "./components/AuthContext";
 
 const PrivateRoute = ({ children, roles }) => {
   const [user, setUser] = useState(null);
@@ -66,47 +67,58 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
+          {/* Default route redirects to home */}
           <Route path="/" element={<Navigate to="/home" />} />
+          
+          {/* General Routes */}
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/districts" element={<Districts />} />
-          <Route path="/Adminpanel" element={<Adminpanel />} />
-          <Route path="/Createjob" element={<Createjob />} />
-          <Route path="/Viewjob" element={<Viewjob />} />
-          <Route path="/Createtourism" element={<Createtourism />} />
-          <Route path="/Viewtourism" element={<Viewtourism />} />
-          <Route path="/edit-job/:id" element={<Updatejob />} />
-          <Route path="/edit-tourism/:id" element={<Updatetourism />} />
-          <Route path="/Createhospital" element={<Createhospital />} />
-          <Route path="/Viewhospital" element={<Viewhospital />} />
-          <Route path="/edit-hospital/:id" element={<Updatehospital />} />
-          <Route path="/edit-school/:id" element={<Updateschool />} />
-          <Route path="/Createschool" element={<Createschool />} />
-          <Route path="/Viewschool" element={<Viewschool />} />
+          <Route path="/adminpanel" element={<PrivateRoute roles={['admin']}><Adminpanel /></PrivateRoute>} />
+          
+          {/* Job Management */}
+          <Route path="/create-job" element={<PrivateRoute roles={['admin']}><Createjob /></PrivateRoute>} />
+          <Route path="/view-job" element={<Viewjob />} />
+          <Route path="/edit-job/:id" element={<PrivateRoute roles={['admin']}><Updatejob /></PrivateRoute>} />
+          
+          {/* Tourism Management */}
+          <Route path="/create-tourism" element={<PrivateRoute roles={['admin']}><Createtourism /></PrivateRoute>} />
+          <Route path="/view-tourism" element={<Viewtourism />} />
+          <Route path="/edit-tourism/:id" element={<PrivateRoute roles={['admin']}><Updatetourism /></PrivateRoute>} />
+          
+          {/* Hospital Management */}
+          <Route path="/create-hospital" element={<PrivateRoute roles={['admin']}><Createhospital /></PrivateRoute>} />
+          <Route path="/view-hospital" element={<Viewhospital />} />
+          <Route path="/edit-hospital/:id" element={<PrivateRoute roles={['admin']}><Updatehospital /></PrivateRoute>} />
+          
+          {/* School Management */}
+          <Route path="/create-school" element={<PrivateRoute roles={['admin']}><Createschool /></PrivateRoute>} />
+          <Route path="/view-school" element={<Viewschool />} />
+          <Route path="/edit-school/:id" element={<PrivateRoute roles={['admin']}><Updateschool /></PrivateRoute>} />
+
+          {/* District Routes */}
           <Route path="/district/alappuzha" element={<Alappuzha />} />
-          <Route path="/district/Ernakulam" element={<Ernakulam />} />
-          <Route path="/district/Idukki" element={<Idukki />} />
-          <Route path="/district/Kannur" element={<Kannur />} />
-          <Route path="/district/Kasaragod" element={<Kasaragod />} />
-          <Route path="/district/Kottayam" element={<Kottayam />} />
-          <Route path="/district/Kollam" element={<Kollam />} />
-          <Route path="/district/Kozhikode" element={<Kozhikode />} />
-          <Route path="/district/Malappuram" element={<Malappuram />} />
-          <Route path="/district/Palakkad" element={<Palakkad />} />
-          <Route path="/district/Pathanamthitta" element={<Pathanamthitta />} />
-          <Route path="/district/Thiruvananthapuram" element={<Thiruvananthapuram />} />
-          <Route path="/district/Thrissur" element={<Thrissur />} />
-          <Route path="/district/Wayanad" element={<Wayanad />} />
+          <Route path="/district/ernakulam" element={<Ernakulam />} />
+          <Route path="/district/idukki" element={<Idukki />} />
+          <Route path="/district/kannur" element={<Kannur />} />
+          <Route path="/district/kasaragod" element={<Kasaragod />} />
+          <Route path="/district/kottayam" element={<Kottayam />} />
+          <Route path="/district/kollam" element={<Kollam />} />
+          <Route path="/district/kozhikode" element={<Kozhikode />} />
+          <Route path="/district/malappuram" element={<Malappuram />} />
+          <Route path="/district/palakkad" element={<Palakkad />} />
+          <Route path="/district/pathanamthitta" element={<Pathanamthitta />} />
+          <Route path="/district/thiruvananthapuram" element={<Thiruvananthapuram />} />
+          <Route path="/district/thrissur" element={<Thrissur />} />
+          <Route path="/district/wayanad" element={<Wayanad />} />
 
-
-          <Route path="/district/Hospital" element={<Hospital />} />
-          <Route path="/district/School" element={<School />} />
+          {/* Additional Pages */}
           <Route path="/job-vacancy" element={<JobVacancy />} />
-          <Route path="/Tourism" element={<Tourism />} />
-          <Route path="/Hospital" element={<Hospital />} />
-          <Route path="/School" element={<School />} />
-          <Route path="/Jobapply" element={<Jobapply />} />
-          <Route path="/JobApplications" element={<JobApplications />} />
+          <Route path="/tourism" element={<Tourism />} />
+          <Route path="/hospital" element={<Hospital />} />
+          <Route path="/school" element={<School />} />
+          <Route path="/jobapply" element={<Jobapply />} />
+          <Route path="/jobapplications" element={<PrivateRoute roles={['admin']}><JobApplications /></PrivateRoute>} />
         </Routes>
       </Router>
     </AuthProvider>
